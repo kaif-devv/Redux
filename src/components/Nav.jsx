67614c61837bf4ef0b7b9744e7493eb9e39f60fc
Kaif <kaif59059@gmail.com>
2024-01-13 00:40:07 +0530

    button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,17 +18,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 function Nav() {
-    const count = useSelector((store)=>store.basket.ammount)
+  const count = useSelector((store) => store.basket.ammount)
   return (
-    <div className=" px-14 flex justify-between text-2xl font-semibold">
-        <h1>HOME</h1>
-        <div className='flex flex-row'>
-        <img className='h-10 w-10 ' src='https://cdn.iconscout.com/icon/free/png-512/free-cart-393-460366.png?f=webp&w=256'/>
-        <span className='text-2xl 
-rounded-full absolute top-0 right-11   '>{count} </span>
-        </div>
-        
-</div>
+    <div className="px-14 flex justify-between text-2xl font-semibold">
+      <h1>HOME</h1>
+      <div className=''>
+        <img className='h-10 w-10' src='https://cdn.iconscout.com/icon/free/png-512/free-cart-393-460366.png?f=webp&w=256' />
+        <span className='text-xl bg-red-500 text-white border-red-500 rounded-full h-6 w-6 flex items-center justify-center absolute  top-3 right-11'>
+          {count}
+        </span>
+      </div>
+
+
+    </div>
   )
 }
 
