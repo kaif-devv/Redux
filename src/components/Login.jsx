2cddf2bd7fa882f9825231e1bf1b45d8cc41f0ce
Kaif <kaif59059@gmail.com>
2024-01-20 09:10:15 +0530

    ss

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 function Login() {
     const [Lemail, setLemail] = useState('');
     const [Lpassword, setLpassword] = useState('');
+
+    
     return (
         <>
             <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
